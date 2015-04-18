f2829be3800b82da8c7b2b7d1c44e2d8a9c349aa
Tyler H <cronotrigga@outlook.com>
2015-04-18 06:03:37 -0400

    Update main.js

--- www/apis/main.js
+++ www/apis/main.js
@@ -1,5 +1,3 @@
-// I William Taylor-Holubeshen, 000305063 certify that this material is my original work. No other person's work has been used without due acknowledgement. I have not made my work available to anyone else. -->
-
 (function ( $ ) {
 	
 	$.fn.dice = function ( options ) {
